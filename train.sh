b4a0366ce981ae5996e2ae0932fb8ed64cb1e070
Lorenzo Zanetti <zanetti@unibo.it>
2022-11-22 15:34:25 +0100

    correct experiment name

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 eval "$(conda shell.bash hook)"
 conda activate radiography_retrieval
-python3 train.py -ws 2500 -wd 1e-4 -st cosine -e 5 -en ./test_blabla --patch_size 16 
+python3 train.py -ws 2500 -wd 1e-4 -st cosine -e 5 -en ./train_no_label_16x16_patch --patch_size 16 
